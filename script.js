1d22b576176e3aa857f7f11531b69452f47edbda
carot29 <l.bruyere@share.epsb.ca>
2024-11-28 14:38:32 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// INCREMENTING VARIABLES & RANDOM VALUES
+
+// 1. Saving elements as variables cleans up your code.
+
+
+
+// 2. Incrementing. The ++, --, +=, and -= operators are used to update variables without losing what was there before
+
+
+
+// 3. The += operator also works to update strings. Also, the "change" event can remove the need for buttons!
+
+
+
+// 4. Math.random() is used to bring RNG (random number generator) to your applications.
+
+
+
+// Check your understanding
+  // 1. Complete the rest of the incrementing buttons.
+  // 2. Complete the random __ to __ button.
+  // 3. Complete the Random Size button. This button should change the font size of all text in the body to a value between 0 and 75
+  // 4. Complete the Random Color button. This should change the html background to a random rgb color.  Hint:  `rgb(34, 245, 128)` is a value rgb color, where the numbers each range from 0 to 255. You will need three random numbers, then you will need to create a string using those numbers.
+
+  // i.e.  var rgbString = `rgb(34, 245, 128)`;
+  //       document.getElementById('HTML').style.background = rgbString
+
+  // Your job is to make the values random in the above code snippet.
+
+  // 4. Code the reset button. This will reset all variables back to initial values and clear out all input fields and reset span boxes back to dashes. It's like refresing the page without actually refreshing the page.
